c470de53454075f02a8f6e8628f3922e60912ff2
Jake Howard <git@theorangeone.net>
2019-09-29 14:57:00 +0100

    Don't install `gh-pages` globally

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -6,6 +6,6 @@ git config --global push.default simple
 git config --global user.email $(git --no-pager show -s --format='%ae' HEAD)
 git config --global user.name $CIRCLE_USERNAME
 
-npm install -g gh-pages
+npm install gh-pages
 
-gh-pages --dist _site/ --message "Deploy build $CIRCLE_BUILD_NUM - $CIRCLE_SHA1 [ci skip]"
+node_modules/.bin/gh-pages --dist _site/ --message "Deploy build $CIRCLE_BUILD_NUM - $CIRCLE_SHA1 [ci skip]"
