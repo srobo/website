2607b59db63a67ea403a6a7f4dd66f6683f63e2a
Peter Law <PeterJCLaw@gmail.com>
2020-11-22 22:52:01 +0000

    Introduce time display localistion for other timezones
    This adds some JavaScript to localise event times.
    
    We only actually apply this for people who aren't in the default
    timezone. Additionally we do some trickery to try to keep the date
    formatting unchanged for english speaking visitors.

---
+++ js/localise-times.js
@@ -0,0 +1,51 @@
+function localise_times(site_timezone) {
+  let options = {
+    day: 'numeric',
+    month: 'long',
+    year: 'numeric',
+    hour: 'numeric',
+    hour12: true,
+    minute: 'numeric',
+    dayPeriod: 'short',
+  };
+  const date_formatter_with_minutes = Intl.DateTimeFormat(undefined, options);
+  delete options.minute;
+  const date_formatter_without_minutes = Intl.DateTimeFormat(undefined, options);
+
+  if (date_formatter_with_minutes.resolvedOptions().timeZone == site_timezone) {
+    return;
+  }
+
+  document.querySelectorAll("time.time-span").forEach((elem) => {
+    const datetime = elem.parentElement.attributes.datetime.value;
+    if (!datetime) {
+      return;
+    }
+
+    const date = new Date(datetime);
+    const formatter =
+      date.getMinutes() > 0
+        ? date_formatter_with_minutes
+        : date_formatter_without_minutes;
+
+    let parts = formatter.formatToParts(date);
+
+    if (navigator.language.startsWith('en-')) {
+      // For english speaking visitors where the local format is broadly the
+      // same as what our event pages show, attempt to make it exactly the same.
+      parts = parts.map(({ type, value }, index) => {
+        if (type == 'literal') {
+          if (parts[index + 1].type == 'hour' && value == ", ") {
+            value = " at ";
+          }
+          if (parts[index + 1].type == 'dayPeriod' && value == " ") {
+            value = "";
+          }
+        }
+        return { type, value };
+      });
+    }
+
+    elem.textContent = parts.map(({ value }) => value).join("");
+  });
+}
